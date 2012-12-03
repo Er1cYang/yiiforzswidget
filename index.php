71b40001a34f3a7da8bf99c53dca2d40a20b4244
YangDongqi <yy2975@126.com>
2012-12-04 01:39:01 +0800

    initial commit

--- index.php
+++ index.php
@@ -1,13 +1,123 @@
 <?php
 
+$system_path = 'system';
+$application_folder = 'application';
+
+/** 
+ * Ê±Çøtimezone set
+ */
+ini_set ('date.timezone', 'Asia/Shanghai');
+
+/*
+ *---------------------------------------------------------------
+ * APPLICATION ENVIRONMENT 
+ *---------------------------------------------------------------
+ *
+ * You can load different configurations depending on your
+ * current environment. Setting the environment also influences
+ * things like logging and error reporting.
+ *
+ * This can be set to anything, but default usage is:
+ *
+ *     development
+ *     testing
+ *     production
+ *
+ * NOTE: If you change these, also change the error_reporting() code below
+ *
+ */
+	define('ENVIRONMENT', 'development');
+/*
+ *---------------------------------------------------------------
+ * ERROR REPORTING
+ *---------------------------------------------------------------
+ *
+ * Different environments will require different levels of error reporting.
+ * By default development will show errors but testing and live will hide them.
+ */
+
+if (defined('ENVIRONMENT'))
+{
+	switch (ENVIRONMENT)
+	{
+		case 'development':
+			error_reporting(E_ALL);
+		break;
+	
+		case 'testing':
+		case 'production':
+			error_reporting(0);
+		break;
+
+		default:
+			exit('The application environment is not set correctly.');
+	}
+}
+
+/*
+ * -------------------------------------------------------------------
+ *  Now that we know the path, set the main path constants
+ * -------------------------------------------------------------------
+ */
+	//WEB ROOT DIR
+	define('WEB_ROOT', dirname(__FILE__));
+	
+	// The name of THIS file
+	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+
+	// The PHP file extension
+	// this global constant is deprecated.
+	define('EXT', '.php');
+
+	// Path to the system folder
+	define('BASEPATH', str_replace("\\", "/", $system_path));
+
+	// Path to the front controller (this file)
+	define('FCPATH', str_replace(SELF, '', __FILE__));
+
+	// Name of the "system folder"
+	define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
+
+	//APP_NAME
+	define('APP_NAME', 'student');
+
+	//server_name
+	define('SERVER_NAME', 'http://'.APP_NAME.'.dev.zae.zhongsou.com/');
+	
+	//API_KEY
+	define('API_KEY', 'abc');
+	
+	//api_host
+	define('OPEN_HOST', 'http://adminzae.zhongsou.com/');
+
+	// The path to the "application" folder
+	if (is_dir($application_folder))
+	{
+		define('APPPATH', $application_folder.'/');
+	}
+	else
+	{
+		if ( ! is_dir(BASEPATH.$application_folder.'/'))
+		{
+			exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: ".SELF);
+		}
+
+		define('APPPATH', BASEPATH.$application_folder.'/');
+	}
+
+
+
+define('YII_DEV_ENV', $_SERVER['SERVER_ADDR'] != '127.0.0.1' ? 'remote' : 'local'); 
+
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii1112/yii.php';
+$yii=dirname(__FILE__).'/lib/yii/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
